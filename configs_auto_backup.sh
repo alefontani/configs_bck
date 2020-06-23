91b7a0cabbb8ebb6c73312ad27daa88b319ebc78
alessandro fontani <alefontani@tiscali.it>
2020-06-23 15:38:25 +0200

    Automatic commit.

--- configs_auto_backup.sh
+++ configs_auto_backup.sh
@@ -14,8 +14,12 @@
 #       /home/ale/configs_bck IN_MODIFY bash /home/ale/configs_bck/configs_auto_backup.sh
 #
 
+# move in script directory (used if launched qith automatic tools e.g. incrontab
+# that run from another dir)
+cd "$(dirname "$0")"
+
 TMPDIR=$(mktemp -d)
-trap "rm -rf ${TMPDIR}" EXIT
+trap "rm -rf ${TMPDIR}; cd -" EXIT
 SCRIPTNAME="$(basename "$0" .sh)"
 TMP_ERROR="/${TMPDIR}/err_${SCRIPTNAME}"
 
@@ -28,6 +32,7 @@ function check_command() {
 
    if [ $1 -ne 0 ]; then
     do_log error "$(cat ${TMP_ERROR} 2>/dev/null)"
+    cp -f ${TMP_ERROR} /home/ale/Desktop
     rm -f ${TMP_ERROR}
     sync
     exit $1
@@ -38,7 +43,7 @@ function check_command() {
 BRANCH="automatic"
 
 git checkout $BRANCH >${TMP_ERROR} 2>&1
-check_command $? 2>&1
+check_command $?
 
 git add --all >${TMP_ERROR} 2>&1
 check_command $?
