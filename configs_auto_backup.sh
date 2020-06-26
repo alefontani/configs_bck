32a84b982124bcb8d75978274f6e2de7c58d037b
alessandro fontani <alefontani@tiscali.it>
2020-06-26 17:16:16 +0200

    Automatic commit.

--- configs_auto_backup.sh
+++ configs_auto_backup.sh
@@ -14,16 +14,7 @@
 #     /home/ale/configs_bck IN_MODIFY bash -c '/home/ale/configs_bck/configs_auto_backup.sh' ale
 #
 
-touch /home/ale/Desktop/partito
 
-# move in script directory (used if launched qith automatic tools e.g. incrontab
-# that run from another dir)
-cd "$(dirname "$0")"
-
-TMPDIR=$(mktemp -d)
-trap "rm -rf ${TMPDIR}; cd -" EXIT
-SCRIPTNAME="$(basename "$0" .sh)"
-TMP_ERROR="/${TMPDIR}/err_${SCRIPTNAME}"
 
 function check_command() {
   #
@@ -42,18 +33,140 @@ function check_command() {
   rm -f ${TMP_ERROR}
 }
 
-BRANCH="automatic"
+function do_logsetup() {
+  #
+  # Call in int function if you wanto to write logs into ${LOGFILE}
+  #
 
-git checkout $BRANCH >${TMP_ERROR} 2>&1
-check_command $?
+  LOG_TMP=$(tail -n $RETAIN_NUM_LINES $LOGFILE 2>/dev/null) \
+          && echo "${LOG_TMP}" > $LOGFILE
+  exec > >(tee -a $LOGFILE)
+  exec 2>&1
+}
 
-git add --all >${TMP_ERROR} 2>&1
-check_command $?
+function do_log() {
+  #
+  # Function usage:
+  # do_log debug "Waiting for all do_send_manifest_pids"
+  #
 
-git commit -am "Automatic commit." >${TMP_ERROR} 2>&1
-check_command $?
+  local arg="$2"
+  COLUMNS=$(tput cols)
+  case $1 in
+    info)
+      echo -e "[$(date "+%H:%M:%S:%3N")]:    ${NORMAL_COLOR}  [Info] ${NORMAL_COLOR} $arg"
+      ;;
+    success)
+      echo -e "[$(date "+%H:%M:%S:%3N")]: ${GREEN__COLOR}  [Success] ${NORMAL_COLOR} $arg"
+      ;;
+    error)
+      echo -e "[$(date "+%H:%M:%S:%3N")]:   ${RED____COLOR}  [Error] ${NORMAL_COLOR} $arg"
+      ;;
+    warning)
+      echo -e "[$(date "+%H:%M:%S:%3N")]: ${YELLOW_COLOR}  [Warning] ${NORMAL_COLOR} $arg"
+      ;;
+    curlwarn)
+      echo -e "[$(date "+%H:%M:%S:%3N")]: ${PURPLE_COLOR} [CurlWarn] ${NORMAL_COLOR} $arg"
+      ;;
+    title)
+      echo -ne "${WHITE__COLOR}"
+      printf "%*s\n" $COLUMNS | tr ' ' '='
+      printf "%*s\n" $(((${#arg}+$COLUMNS)/2)) "$arg"
+      printf "%*s\n" $COLUMNS | tr ' ' '='
+      echo -ne "${NORMAL_COLOR}"
+      ;;
+    section)
+      echo -ne "${WHITE__COLOR}"
+      printf "%*s\n" $COLUMNS | tr ' ' '-'
+      printf "%*s\n" $(((${#arg}+$COLUMNS)/2)) "$arg"
+      printf "%*s\n" $COLUMNS | tr ' ' '-'
+      echo -ne "${NORMAL_COLOR}"
+      ;;
+    debug)
+      if [ "$DEBUG" = "true" ]; then
+        echo -e "[$(date "+%H:%M:%S:%3N")]:   ${CYAN___COLOR}  [Debug] ${NORMAL_COLOR} $arg"
+      fi
+    # *)
+    #   echo -e "[$(date "+%H:%M:%S:%3N")]:                [SpareInfo] $arg"
+    #   ;;
+  esac
+}
 
-git push origin $BRANCH >${TMP_ERROR} 2>&1
-check_command $?
+case "$item" in
+  1)
+    echo "case 1"
+  ;;
+  2|3)
+    echo "case 2 or 3"
+  ;;
+  *)
+    echo "default"
+  ;;
+esac
 
+
+function exitFunction() {
+  #
+  # exit stuff useful for trap and exit
+  #
+
+  rm -rf ${TMPDIR}
+  cd -
+}
+
+function do_init() {
+
+  # move in script directory (used if launched qith automatic tools e.g. incrontab
+  # that run from another dir)
+  cd "$(dirname "$0")"
+
+  TMPDIR=$(mktemp -d)
+  trap "exitFunction;" EXIT
+
+  SCRIPTNAME="$(basename "$0" .sh)"
+  LOGFILE="${SCRIPTNAME}.log"
+  RETAIN_NUM_LINES=1000
+  TMP_ERROR="/${TMPDIR}/err_${SCRIPTNAME}"
+
+  do_logsetup
+}
+
+###############################################################################
+# main # main # main # main # main # main # main # main # main # main # main #
+###############################################################################
+function main()
+{
+  do_init
+
+  do_log title "Starting $SCRIPTNAME"
+
+  touch /home/ale/Desktop/configs_auto_backup_partito
+
+  if [ $(git status --porcelain | wc -l) -eq "0" ]; then
+    do_log info "Git repo is clean. Exiting."
+    return
+  fi
+
+  BRANCH="automatic"
+
+  git ls-remote >${TMP_ERROR} 2>&1
+  check_command $?
+
+  git checkout $BRANCH >${TMP_ERROR} 2>&1
+  check_command $?
+
+  git add --all >${TMP_ERROR} 2>&1
+  check_command $?
+
+  git commit -am "Automatic commit." >${TMP_ERROR} 2>&1
+  check_command $?
+
+  git push origin $BRANCH >${TMP_ERROR} 2>&1
+  check_command $?
+
+
+  do_log title "End"
+}
+
+main
 exit 0
